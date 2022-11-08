67aad08ebc04add3a25c28e8abf77ac28241ccf6
Jasel <patriajasel@gmail.com>
2022-11-08 14:14:16 +0800

    Adding to New Recipe to Firestore

---
+++ js/addToFirestore.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAbssBn3kn-Yl3JDPhAaveGl-_LQKK7iqU",
+    authDomain: "cookbookcapstone.firebaseapp.com",
+    databaseURL: "https://cookbookcapstone-default-rtdb.firebaseio.com",
+    projectId: "cookbookcapstone",
+    storageBucket: "cookbookcapstone.appspot.com",
+    messagingSenderId: "785262813455",
+    appId: "1:785262813455:web:a5861d5beca474b37e1e7a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
